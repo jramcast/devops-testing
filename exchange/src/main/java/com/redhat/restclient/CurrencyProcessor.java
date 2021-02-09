233e6e2a3ada8a4ceb9dbe9de04eb3117f67364d
Jaime Ramírez <jaime.ramirez@redhat.com>
2021-02-09 12:12:52 +0100

    fixes

--- exchange/src/main/java/com/redhat/restclient/CurrencyProcessor.java
+++ exchange/src/main/java/com/redhat/restclient/CurrencyProcessor.java
@@ -9,15 +9,13 @@ public class CurrencyProcessor {
 
     public Currency getFirst(List<Currency> currencies, String target) {
         Currency latestCurrency = currencies.get(0);
-        try {
-            if(target.equals("USD")) {
-                latestCurrency.setSymbol("$");
-            } else {
-                latestCurrency.setSymbol("€");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        if(target.equals("USD")) {
+            latestCurrency.setSymbol("$");
+        } else {
+            latestCurrency.setSymbol("€");
         }
+
         return latestCurrency;
     }
 
--- history/tests/unit/rates/RatesService.test.js
+++ history/tests/unit/rates/RatesService.test.js
@@ -2,16 +2,6 @@ const RatesService = require("../../../source/rates/RatesService");
 
 describe("RatesService", () => {
 
-    test("returns rates retrieved from a rates repository", () => {
-        const repository = {
-            load: () => [{ value: 1 }, { value: 2 }, { value: 3 } ]
-        };
-
-        const service = new RatesService(repository);
-        const rates = service.loadRatesHistory("EUR", "USD");
-        expect(rates).toEqual([{ value: 1 }, { value: 2 }, { value: 3 } ]);
-    });
-
     test("throws an exception if the repository returns null", () => {
         function invalidScenario() {
             const repository = {
@@ -22,6 +12,38 @@ describe("RatesService", () => {
         }
 
         expect(invalidScenario).toThrow(/Unknown currency conversion/i);
+    });
+
+    test("returns a list of currency exchange rates read from the repository", () => {
+        // Given
+        const repository = {
+            load: () => [{ value: 1 }, { value: 5}]
+        };
+        const service =  new RatesService(repository);
+
+        // When
+        const rates = service.loadRatesHistory("EUR", "USD");
+
+        // Then
+        expect(rates[0].value).toBe(1);
+        expect(rates[1].value).toBe(5);
+    });
+
+
+    test("returns each data point with a date", () => {
+        // Given
+        const repository = {
+            load: () => [{ value: 1 }, { value: 5}]
+        };
+        const service =  new RatesService(repository);
+
+        // When
+        const rates = service.loadRatesHistory("EUR", "USD");
+
+        // Then
+        for (const rate of rates) {
+            expect(rate.date).toBeInstanceOf(Date);
+        }
     });
 
 });
